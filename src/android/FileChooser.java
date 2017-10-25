bba121896f0f069ce429a325ba4e4499e8b18354
Tom Bell <tomb@tomb.io>
2017-10-25 14:12:48 +0100

    Add file name as part of the success params

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -2,7 +2,9 @@ package com.megster.cordova;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.util.Log;
 
 import org.apache.cordova.CordovaArgs;
@@ -10,17 +12,18 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FileChooser extends CordovaPlugin {
+    private static final String ACTION_OPEN = "open";
 
     private static final String TAG = "FileChooser";
-    private static final String ACTION_OPEN = "open";
     private static final int PICK_FILE_REQUEST = 1;
+
     CallbackContext callback;
 
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
-
         if (action.equals(ACTION_OPEN)) {
             chooseFile(callbackContext);
             return true;
@@ -30,9 +33,6 @@ public class FileChooser extends CordovaPlugin {
     }
 
     public void chooseFile(CallbackContext callbackContext) {
-
-        // type and title should be configurable
-
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -49,32 +49,31 @@ public class FileChooser extends CordovaPlugin {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (requestCode == PICK_FILE_REQUEST && callback != null) {
-
             if (resultCode == Activity.RESULT_OK) {
-
                 Uri uri = data.getData();
 
                 if (uri != null) {
-
                     Log.w(TAG, uri.toString());
-                    callback.success(uri.toString());
+                    Cursor cursor = this.cordova.getActivity().getContentResolver().query(uri, null, null, null, null);
+                    int nameIdx = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    cursor.moveToFirst();
 
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put("filePath", uri.toString());
+                        json.put("fileName", cursor.getString(nameIdx));
+                        callback.success(json);
+                    } catch (JSONException e) {
+                        callback.error("Unable to create JSON object");
+                    }
                 } else {
-
                     callback.error("File uri was null");
-
                 }
-
             } else if (resultCode == Activity.RESULT_CANCELED) {
-
-                // TODO NO_RESULT or error callback?
                 PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
                 callback.sendPluginResult(pluginResult);
-
             } else {
-
                 callback.error(resultCode);
             }
         }
